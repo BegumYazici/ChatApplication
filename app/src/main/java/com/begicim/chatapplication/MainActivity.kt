a8aa85d7ff4c2db99628eb1c86ef0d82dcb65f66
Begum Yazici <yazicibegum@gmail.com>
2021-10-19 11:49:56 +0100

    Add container into the main activity layout

--- app/src/main/java/com/begicim/chatapplication/MainActivity.kt
+++ app/src/main/java/com/begicim/chatapplication/MainActivity.kt
@@ -1,11 +1,25 @@
 package com.begicim.chatapplication
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.begicim.chatapplication.ui.ChatFragment
+import com.begicim.chatapplication.ui.ChatFragment.Companion.CHAT_FRAGMENT_TAG
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val chatFragment : ChatFragment = if(savedInstanceState == null) {
+            val chatFragment = ChatFragment()
+            supportFragmentManager.beginTransaction()
+                .replace(R.id.fragment_container_view, chatFragment, CHAT_FRAGMENT_TAG)
+                .commit()
+
+            chatFragment
+        } else {
+            supportFragmentManager.findFragmentByTag(CHAT_FRAGMENT_TAG) as ChatFragment
+        }
+
     }
 }
