d341d2d9e8331bc5b49dfa1e2edef74248c6246e
Begum Yazici <yazicibegum@gmail.com>
2021-10-19 11:50:16 +0100

    Add Chat fragment

--- app/src/main/java/com/begicim/chatapplication/ui/ChatFragment.kt
+++ app/src/main/java/com/begicim/chatapplication/ui/ChatFragment.kt
@@ -1,60 +1,68 @@
 package com.begicim.chatapplication.ui
 
 import android.os.Bundle
-import androidx.fragment.app.Fragment
-import android.view.LayoutInflater
 import android.view.View
-import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import androidx.lifecycle.ViewModelProvider
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 import com.begicim.chatapplication.R
+import com.begicim.chatapplication.ui.adapter.ChatAdapter
+import com.begicim.chatapplication.ui.model.MessageUIModel
 
-// TODO: Rename parameter arguments, choose names that match
-// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-private const val ARG_PARAM1 = "param1"
-private const val ARG_PARAM2 = "param2"
 
-/**
- * A simple [Fragment] subclass.
- * Use the [ChatFragment.newInstance] factory method to
- * create an instance of this fragment.
- */
-class ChatFragment : Fragment() {
-    // TODO: Rename and change types of parameters
-    private var param1: String? = null
-    private var param2: String? = null
+class ChatFragment : Fragment(R.layout.fragment_chat) {
+
+    private lateinit var chatViewModel: ChatViewModel
+
+    private val chatAdapter = ChatAdapter(
+        listOf(
+            MessageUIModel(
+                "message1 dffdfdfd dffdfdfd dfdffd begum saat 12 yı gectı eyvah",
+                "12.12.21",
+                false,
+                false
+            ),
+            MessageUIModel("message2-sender", "12.12.21", true, false),
+            MessageUIModel("message3", "12.12.21", false, false),
+            MessageUIModel("message4-sender", "12.12.21", true, true),
+            MessageUIModel("message5", "12.12.21", false, false),
+            MessageUIModel("message6-sender", "12.12.21", true, false),
+            MessageUIModel("message3", "12.12.21", false, false),
+            MessageUIModel("message4-sender", "12.12.21", true, true),
+            MessageUIModel("message5", "12.12.21", false, false),
+            MessageUIModel("message6-sender", "12.12.21", true, false),
+            MessageUIModel("message3", "12.12.21", false, false),
+            MessageUIModel("message4-sender", "12.12.21", true, true),
+            MessageUIModel("message5", "12.12.21", false, false),
+            MessageUIModel("message6-sender", "12.12.21", true, false),
+            MessageUIModel("message5", "12.12.21", false, false),
+            MessageUIModel("message6-sender", "12.12.21", true, false)
+        )
+    )
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        arguments?.let {
-            param1 = it.getString(ARG_PARAM1)
-            param2 = it.getString(ARG_PARAM2)
-        }
+
+        chatViewModel = ViewModelProvider(this).get(ChatViewModel::class.java)
     }
 
-    override fun onCreateView(
-        inflater: LayoutInflater, container: ViewGroup?,
-        savedInstanceState: Bundle?
-    ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_chat, container, false)
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        val chatRecyclerview = view.findViewById<RecyclerView>(R.id.chatRecyclerview)
+
+        val linearLayoutManager = LinearLayoutManager(context).apply {
+            reverseLayout = true
+        }
+
+        chatRecyclerview.layoutManager = linearLayoutManager
+        chatRecyclerview.adapter = chatAdapter
+
+        chatAdapter.notifyDataSetChanged()
     }
 
     companion object {
-        /**
-         * Use this factory method to create a new instance of
-         * this fragment using the provided parameters.
-         *
-         * @param param1 Parameter 1.
-         * @param param2 Parameter 2.
-         * @return A new instance of fragment ChatFragment.
-         */
-        // TODO: Rename and change types and number of parameters
-        @JvmStatic
-        fun newInstance(param1: String, param2: String) =
-            ChatFragment().apply {
-                arguments = Bundle().apply {
-                    putString(ARG_PARAM1, param1)
-                    putString(ARG_PARAM2, param2)
-                }
-            }
+        const val CHAT_FRAGMENT_TAG = "Chat Fragment"
     }
 }
