71ac3188d8ddfebc27ce0c2145724161c170b7ae
Begum Yazici <yazicibegum@gmail.com>
2021-10-19 21:08:38 +0100

    Add date format const

--- app/src/main/java/com/begicim/chatapplication/util/DateFormat.kt
+++ app/src/main/java/com/begicim/chatapplication/util/DateFormat.kt
@@ -1,4 +1,4 @@
 package com.begicim.chatapplication.util
 
-class DateFormat {
-}
+const val DATE_FORMAT_YEAR_MONTH_DAY_HOUR_MINUTE = "yyyy-MM-dd'T'HH:mm"
+const val DATE_FORMAT_DAY_HOUR_MINUTE = "EEEE, HH:mm"
